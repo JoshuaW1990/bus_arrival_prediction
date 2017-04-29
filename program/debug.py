9d7cd8a4faa8b2541fde6c68622ae8cee3a34322
JoshuaW1990 <Jun.Wang@tufts.edu>
2017-04-28 21:30:05 -0500

    update

--- program/debug.py
+++ program/debug.py
@@ -42,5 +42,24 @@ import os
 # final_baseline_result = baseline_result[baseline_result.shape_id.isin(shape_id_set)]
 # final_baseline_result.to_csv('full_baseline_result.csv')
 
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 
+# db = pymysql.connect(host='localhost', user='root', password='Wj15029054380', db='bus_prediction')
+
+
+# read the full history data into database
+
+
+
+engine = create_engine('postgresql://joshuaw:Wj15029054380@localhost:5432/bus_prediction', echo=False)
+full_api_data = pd.read_csv('full_api_data.csv')
+full_api_data.drop(['Unnamed: 0'], axis=1, inplace=True)
+full_api_data.to_sql(name='full_api_data', con=engine, if_exists='replace', index_label='id')
+print "complete save full_api_data into database"
+
+full_api_data_baseline3 = pd.read_csv('tmp.csv')
+full_api_data_baseline3.drop(['Unnamed: 0'], axis=1, inplace=True)
+full_api_data_baseline3.to_sql(name='full_api_baseline', con=engine, if_exists='replace', index_label='id')
 
